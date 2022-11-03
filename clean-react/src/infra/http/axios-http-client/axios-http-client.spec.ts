e4deb212bb046f00f9e1fbab4f310ab6d1c81b42
Weverson Neri <weversonneri@outlook.com>
2022-11-02 21:54:55 -0300

    refactor: muve post request creation to a factory method

--- clean-react/src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ clean-react/src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import {faker} from "@faker-js/faker"
 import { AxiosHttpClient } from './axios-http-client'
+import { HttpPostParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios> //acesso recursos do axios, cacheado
@@ -9,12 +10,17 @@ const makeSut = (): AxiosHttpClient => {
  return new AxiosHttpClient()
 }
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.science.chemicalElement() 
+})
+
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct URL', async() => {
-    const url = faker.internet.url()
+    const req = mockPostRequest()
     const sut = makeSut()
-    await sut.post({ url })
+    await sut.post(req)
 
-    expect(mockedAxios.post).toHaveBeenCalledWith(url);
+    expect(mockedAxios.post).toHaveBeenCalledWith(req.url);
   })
 })
