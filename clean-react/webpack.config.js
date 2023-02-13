4bfd29a403df3072f0bfb3a5c14982124e789fd7
Weverson Neri <weversonneri@outlook.com>
2023-02-13 20:17:31 -0300

    chore: config sass

--- clean-react/webpack.config.js
+++ clean-react/webpack.config.js
@@ -10,15 +10,35 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
   },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      },
+      { loader: 'sass-loader' }
+      ]
+    }]
+  },
   devServer: {
     contentBase: './public',
     writeToDisk: true, // gera o bundle e levanta o servidor,
-    historyApiFallback: true //pro react mapear qualquer rota, devServer entende somente o index
+    historyApiFallback: true // pro react mapear qualquer rota, devServer entende somente o index
   },
   externals: { // o que tiver aqui nao ficara no bundle
     react: 'React',
